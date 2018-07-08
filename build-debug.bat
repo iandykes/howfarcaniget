50da5b806aae3d03d8ef3a3f590a458f4ae52a45
Ian Dykes <ian.dykes@gmail.com>
2018-07-08 10:09:31 +0100

    Added build variables for windows build

---
+++ build-debug.bat
@@ -0,0 +1,5 @@
+@echo on
+set BUILD_BUILDNUMBER=0.0
+set BUILD_SOURCEVERSION=no-commit-hash
+
+build-win.bat
---
+++ build-win.bat
@@ -0,0 +1,2 @@
+@echo on
+go build -ldflags "-X 'main.version=%BUILD_BUILDNUMBER%' -X 'main.buildDate=%date%T%time%' -X 'main.commitHash=%BUILD_SOURCEVERSION%'"
