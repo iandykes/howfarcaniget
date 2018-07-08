e87a281c2573805acd169e73672a12ea6038d36c
Ian Dykes <ian.dykes@gmail.com>
2018-07-08 21:48:54 +0100

    Added version number to UI

--- build-debug.bat
+++ build-debug.bat
@@ -1,5 +1,6 @@
-@echo on
-set BUILD_BUILDNUMBER=0.0
-set BUILD_SOURCEVERSION=no-commit-hash
+@echo off
+:: These are set from VSTS normally
+set BUILD_BUILDNUMBER=12.34.56
+set BUILD_SOURCEVERSION=local-dev-build
 
 build-win.bat
--- build-win.bat
+++ build-win.bat
@@ -1,2 +1,2 @@
-@echo on
+@echo off
 go build -ldflags "-X 'main.version=%BUILD_BUILDNUMBER%' -X 'main.buildDate=%date%T%time%' -X 'main.commitHash=%BUILD_SOURCEVERSION%'"
